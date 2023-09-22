5e5b33c48a86886a0e5be789e55772b2835de0f2
Alex <phoenix.trade2307@gmail.com>
2023-09-22 23:43:32 +0300

    method bind for callbacks in props

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addNewPost, changeTextPost, state, StateType, subscribe} from './redux/state'
+import {StateType, store} from './redux/state'
 
 const rerenderEntireTreeForIndex = (state: StateType) => {
     ReactDOM.render(
         <App
             state={state}
-            addPostCallback={addNewPost}
-            changeTextPost={changeTextPost}/>,
+            addPostCallback={store.addNewPost.bind(store)}
+            changeTextPost={store.changeTextPost.bind(store)}/>,
         document.getElementById('root')
     );
 }
 
-rerenderEntireTreeForIndex(state);
+rerenderEntireTreeForIndex(store.getState());
 
-subscribe(rerenderEntireTreeForIndex)
+store.subscribe(rerenderEntireTreeForIndex)
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,76 +1,154 @@
 import {v1} from "uuid";
 
-let rerenderEntireTreeForState = (state: StateType) => {
-    console.log('dfgdgd')
-}
+// let rerenderEntireTreeForState = (state: StateType) => {
+//     console.log('dfgdgd')
+// }
+
+// export type StateType = typeof state
+//
+// export const state = {
+//     profilePage: {
+//         postsData: [
+//             {id: v1(), postContent: 'Gloria to Ukraine', likesCount: 13},
+//             {id: v1(), postContent: 'Gloria to heroes', likesCount: 12},
+//             {id: v1(), postContent: 'Death for enemies', likesCount: 13},
+//         ],
+//         newTextPost: ''
+//     },
+//     messagesPage: {
+//         dialogsData: [
+//             {id: v1(), name: 'Alex'},
+//             {id: v1(), name: 'Nika'},
+//             {id: v1(), name: 'Natalie'},
+//             {id: v1(), name: 'Artur'},
+//             {id: v1(), name: 'Oleg'}
+//         ],
+//         messagesData: [
+//             {id: v1(), messageText: 'Hi'},
+//             {id: v1(), messageText: 'How are you?'},
+//             {id: v1(), messageText: 'Fine'},
+//         ]
+//
+//     },
+//     newsPage: {},
+//     musicPage: {},
+//     settingsPage: {}
+// }
+//
+// // Functions for event processing
+//
+// export const addNewPost = () => {
+//     let newPost: PostType = {
+//         id: v1(),
+//         postContent: state.profilePage.newTextPost,
+//         likesCount: 0
+//     }
+//     state.profilePage.postsData.push(newPost)
+//     rerenderEntireTreeForState(state)
+//     state.profilePage.newTextPost = ''
+//
+// }
+//
+// export const changeTextPost = (changedValue: string) => {
+//     state.profilePage.newTextPost = changedValue
+//     rerenderEntireTreeForState(state)
+// }
+//
+// // Functions for rerender
+//
+// export const subscribe = (observer: (state:StateType) => void) => {
+//     rerenderEntireTreeForState = observer
+// }
+
+
+// Store
 export type DialogType = {
     id: string
     name: string
 }
-
 export type MessageType = {
     id: string
     messageText: string
 }
-
 export type PostType = {
     id: string
     postContent: string
     likesCount: number
 }
-
-export type StateType = typeof state
-
-export const state = {
+export type StateType = {
     profilePage: {
-        postsData: [
-            {id: v1(), postContent: 'Gloria to Ukraine', likesCount: 13},
-            {id: v1(), postContent: 'Gloria to heroes', likesCount: 12},
-            {id: v1(), postContent: 'Death for enemies', likesCount: 13},
-        ],
-        newTextPost: ''
-    },
+        postsData: PostType[],
+        newTextPost: string
+    }
     messagesPage: {
-        dialogsData: [
-            {id: v1(), name: 'Alex'},
-            {id: v1(), name: 'Nika'},
-            {id: v1(), name: 'Natalie'},
-            {id: v1(), name: 'Artur'},
-            {id: v1(), name: 'Oleg'}
-        ],
-        messagesData: [
-            {id: v1(), messageText: 'Hi'},
-            {id: v1(), messageText: 'How are you?'},
-            {id: v1(), messageText: 'Fine'},
-        ]
-
-    },
-    newsPage: {},
-    musicPage: {},
+        dialogsData: DialogType[],
+        messagesData: MessageType[]
+    }
+    newsPage: {}
+    musicPage: {}
     settingsPage: {}
 }
 
-// Functions for event processing
 
-export const addNewPost = () => {
-    let newPost: PostType = {
-        id: v1(),
-        postContent: state.profilePage.newTextPost,
-        likesCount: 0
+export type StoreType = {
+    _state: StateType
+    getState: () => StateType
+    _callSubscriber: (state: StateType) => void
+    addNewPost: () => void
+    changeTextPost: (changedValue: string) => void
+    subscribe: (callback: (state: StateType) => void) => void
+}
+
+export const store: StoreType = {
+    _state: {
+        profilePage: {
+            postsData: [
+                {id: v1(), postContent: 'Gloria to Ukraine', likesCount: 13},
+                {id: v1(), postContent: 'Gloria to heroes', likesCount: 12},
+                {id: v1(), postContent: 'Death for enemies', likesCount: 13},
+            ],
+            newTextPost: ''
+        },
+        messagesPage: {
+            dialogsData: [
+                {id: v1(), name: 'Alex'},
+                {id: v1(), name: 'Nika'},
+                {id: v1(), name: 'Natalie'},
+                {id: v1(), name: 'Artur'},
+                {id: v1(), name: 'Oleg'}
+            ],
+            messagesData: [
+                {id: v1(), messageText: 'Hi'},
+                {id: v1(), messageText: 'How are you?'},
+                {id: v1(), messageText: 'Fine'},
+            ]
+
+        },
+        newsPage: {},
+        musicPage: {},
+        settingsPage: {}
+    },
+    getState() {
+        return this._state
+    },
+    _callSubscriber(state: StateType) {
+        console.log('dfgdgd')
+    },
+    addNewPost() {
+        let newPost: PostType = {
+            id: v1(),
+            postContent: this._state.profilePage.newTextPost,
+            likesCount: 0
+        }
+        this._state.profilePage.postsData.push(newPost)
+        this._callSubscriber(this._state)
+        this._state.profilePage.newTextPost = ''
+    },
+    changeTextPost(changedValue: string) {
+        this._state.profilePage.newTextPost = changedValue
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer: (state: StateType) => void) {
+        this._callSubscriber = observer
     }
-    state.profilePage.postsData.push(newPost)
-    rerenderEntireTreeForState(state)
-    state.profilePage.newTextPost = ''
-
-}
-
-export const changeTextPost = (changedValue: string) => {
-    state.profilePage.newTextPost = changedValue
-    rerenderEntireTreeForState(state)
-}
-
-// Functions for rerender
-
-export const subscribe = (observer: (state:StateType) => void) => {
-    rerenderEntireTreeForState = observer
 }
