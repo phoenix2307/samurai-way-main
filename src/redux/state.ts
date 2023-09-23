1916a4e67ef19b6d274720e6dcf71c3ad865d133
Alex <phoenix.trade2307@gmail.com>
2023-09-23 20:25:55 +0300

    optimization of work with types

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,66 +1,5 @@
 import {v1} from "uuid";
 
-// let rerenderEntireTreeForState = (state: StateType) => {
-//     console.log('dfgdgd')
-// }
-
-// export type StateType = typeof state
-//
-// export const state = {
-//     profilePage: {
-//         postsData: [
-//             {id: v1(), postContent: 'Gloria to Ukraine', likesCount: 13},
-//             {id: v1(), postContent: 'Gloria to heroes', likesCount: 12},
-//             {id: v1(), postContent: 'Death for enemies', likesCount: 13},
-//         ],
-//         newTextPost: ''
-//     },
-//     messagesPage: {
-//         dialogsData: [
-//             {id: v1(), name: 'Alex'},
-//             {id: v1(), name: 'Nika'},
-//             {id: v1(), name: 'Natalie'},
-//             {id: v1(), name: 'Artur'},
-//             {id: v1(), name: 'Oleg'}
-//         ],
-//         messagesData: [
-//             {id: v1(), messageText: 'Hi'},
-//             {id: v1(), messageText: 'How are you?'},
-//             {id: v1(), messageText: 'Fine'},
-//         ]
-//
-//     },
-//     newsPage: {},
-//     musicPage: {},
-//     settingsPage: {}
-// }
-//
-// // Functions for event processing
-//
-// export const addNewPost = () => {
-//     let newPost: PostType = {
-//         id: v1(),
-//         postContent: state.profilePage.newTextPost,
-//         likesCount: 0
-//     }
-//     state.profilePage.postsData.push(newPost)
-//     rerenderEntireTreeForState(state)
-//     state.profilePage.newTextPost = ''
-//
-// }
-//
-// export const changeTextPost = (changedValue: string) => {
-//     state.profilePage.newTextPost = changedValue
-//     rerenderEntireTreeForState(state)
-// }
-//
-// // Functions for rerender
-//
-// export const subscribe = (observer: (state:StateType) => void) => {
-//     rerenderEntireTreeForState = observer
-// }
-
-
 // Store
 export type DialogType = {
     id: string
@@ -93,33 +32,25 @@ export type StoreType = {
     _state: StateType
     getState: () => StateType
     _callSubscriber: (state: StateType) => void
-    // addNewPost: () => void
-    // changeTextPost: (changedValue: string) => void
     subscribe: (callback: (state: StateType) => void) => void
     dispatch: (action: ActionsType) => void
 }
 
-type AddPostActionType = {
-    type: 'ADD-POST'
-}
+type AddPostActionType = ReturnType<typeof addPostAC>
 
-type ChangeActionType = {
-    type: 'CHANGE-TEXT-POST'
-    changedValue: string
-}
-export type ActionsType = AddPostActionType | ChangeActionType
+type ChangeTextPostActionType = ReturnType<typeof changeTextPostAC>
+export type ActionsType = AddPostActionType | ChangeTextPostActionType
 
-export const addPostAC = (): AddPostActionType => {
+export const addPostAC = () => {
     return {
         type: "ADD-POST"
-    }
+    } as const
 }
-
-export const changeTextPostAC = (changedValue: string): ChangeActionType => {
+export const changeTextPostAC = (changedValue: string) => {
     return{
         type: "CHANGE-TEXT-POST",
         changedValue: changedValue
-    }
+    } as const
 }
 
 export const store: StoreType = {
@@ -158,20 +89,6 @@ export const store: StoreType = {
     _callSubscriber(state: StateType) {
         console.log('dfgdgd')
     },
-    // addNewPost() {
-    //     let newPost: PostType = {
-    //         id: v1(),
-    //         postContent: this._state.profilePage.newTextPost,
-    //         likesCount: 0
-    //     }
-    //     this._state.profilePage.postsData.push(newPost)
-    //     this._callSubscriber(this._state)
-    //     this._state.profilePage.newTextPost = ''
-    // },
-    // changeTextPost(changedValue: string) {
-    //     this._state.profilePage.newTextPost = changedValue
-    //     this._callSubscriber(this._state)
-    // },
     subscribe(observer: (state: StateType) => void) {
         this._callSubscriber = observer
     },
